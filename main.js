f6aa4a604dafa2146c2c51a19a0a9008f32f10cc
Jimmy Thigpen <jmthigpen@me.com>
2015-01-26 17:44:14 -0500

    afternoon update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var results = rawCharlestonData.results;
+  // var results = rawCharlestonData.results;
 
   $(document).ready(function() {
 
@@ -11,23 +11,42 @@
     var $list = $('.results-list');
 
     //
-    // Sorted by price
+    // Listen for Button Click and create new search term URL
     //
+    $("button").on("click", updateURL);
 
-    $(".dropdown").change(function(sortStuff) {
-      if ($(".dropdown option:selected").text() == "Lowest Price") {
-        results = _.sortBy(results, function(item) {
-          return +item.price
+    function updateURL() {
+      var searchTerm = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=" + $(".search-term").val().replace(/ /g, '+') + "&includes=Images,Shop&sort_on=score&limit=9";
+      //
+      // Get JSON data from Etsy
+      //
+      $.ajax({
+        url: searchTerm,
+        type: "GET",
+        dataType: 'jsonp'
+      }).done(function(updatedResults) {
+        renderListings(updatedResults.results);
+
+        //
+        // Sort Dropdown
+        //
+
+        $(".dropdown").change(function(sortStuff) {
+          if ($(".dropdown option:selected").text() == "Lowest Price") {
+            updatedResults.results = _.sortBy(updatedResults.results, function(item) {
+              return +item.price;
+            });
+          } else if ($(".dropdown option:selected").text() == "Highest Price") {
+            updatedResults.results = _.sortBy(updatedResults.results, function(item) {
+              return +item.price;
+            }).reverse();
+          } else if ($(".dropdown option:selected").text() == "Relevance") {
+            updatedResults.results = updateURL(updatedResults);
+          }
+          renderListings(updatedResults.results);
         });
-      } else if ($(".dropdown option:selected").text() == "Highest Price") {
-        results = _.sortBy(results, function(item) {
-          return +item.price;
-        }).reverse();
-      } else if ($(".dropdown option:selected").text() == "Relevance") {
-        results = rawCharlestonData.results;
-      }
-      renderListings(results);
-    });
+      });
+    }
 
     function renderListings(data) {
       $list.empty();
@@ -36,29 +55,13 @@
           title: result.title,
           cost: result.price,
           shop: result.Shop.shop_name,
-          image: result.Images[1].url_170x135,
+          image: result.Images[0].url_170x135,
           url: result.url,
           currency: result.Shop.currency_code
         });
         $list.append(resultText);
       });
     }
-
-    //
-    // Default / Relevance View
-    //
-
-    results.forEach(function(result) {
-      var resultText = renderTemplate('results-item', {
-        title: result.title,
-        cost: result.price,
-        shop: result.Shop.shop_name,
-        image: result.Images[1].url_170x135,
-        url: result.url,
-        currency: result.Shop.currency_code
-      });
-      $list.append(resultText);
-    });
 
   });
 
