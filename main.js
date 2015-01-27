2407d16cb26d189659af94370e9843e3235a569b
Jimmy Thigpen <jmthigpen@me.com>
2015-01-27 09:01:24 -0500

    morning update

--- main.js
+++ main.js
@@ -9,6 +9,7 @@
     // links html element div with .results-list class
     //
     var $list = $('.results-list');
+    var sortedBy = "score";
 
     //
     // Listen for Button Click and create new search term URL
@@ -16,7 +17,8 @@
     $("button").on("click", updateURL);
 
     function updateURL() {
-      var searchTerm = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=" + $(".search-term").val().replace(/ /g, '+') + "&includes=Images,Shop&sort_on=score&limit=9";
+      var searchTerm = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=" + $(".search-term").val().replace(/ /g, '+') + "&includes=Images,Shop&sort_on=" + sortedBy + "&limit=9";
+
       //
       // Get JSON data from Etsy
       //
@@ -25,27 +27,30 @@
         type: "GET",
         dataType: 'jsonp'
       }).done(function(updatedResults) {
+        if ($(".dropdown option:selected").text() == "Lowest Price") {
+          updatedResults.results = updatedResults.results.reverse();
+        }
         renderListings(updatedResults.results);
 
-        //
-        // Sort Dropdown
-        //
-
-        $(".dropdown").change(function(sortStuff) {
-          if ($(".dropdown option:selected").text() == "Lowest Price") {
-            updatedResults.results = _.sortBy(updatedResults.results, function(item) {
-              return +item.price;
-            });
-          } else if ($(".dropdown option:selected").text() == "Highest Price") {
-            updatedResults.results = _.sortBy(updatedResults.results, function(item) {
-              return +item.price;
-            }).reverse();
-          } else if ($(".dropdown option:selected").text() == "Relevance") {
-            updatedResults.results = updateURL(updatedResults);
-          }
-          renderListings(updatedResults.results);
-        });
       });
+      //
+      // Sort Dropdown
+      //
+
+      $(".dropdown").change(function(sortStuff) {
+
+        if ($(".dropdown option:selected").text() == "Lowest Price") {
+          sortedBy = "price&sort_order=up";
+        } else if ($(".dropdown option:selected").text() == "Highest Price") {
+          sortedBy = "price&sort_order=down";
+        } else if ($(".dropdown option:selected").text() == "Relevance") {
+          sortedBy = "score";
+
+        }
+        updateURL();
+
+      });
+
     }
 
     function renderListings(data) {
